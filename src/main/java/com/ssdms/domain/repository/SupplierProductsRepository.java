604f4d3f26691b87f3d7fe4f9d4a001d105805c6
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-01-12 22:07:05 -0300

    feat: resolve n+1 problem in supplier_products

--- src/main/java/com/ssdms/domain/repository/SupplierProductsRepository.java
+++ src/main/java/com/ssdms/domain/repository/SupplierProductsRepository.java
@@ -1,9 +1,15 @@
 package com.ssdms.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ssdms.domain.model.SupplierProducts;
 
 public interface SupplierProductsRepository extends JpaRepository<SupplierProducts, Integer> {
 
+	@Query("from SupplierProducts s join s.provider join s.product p join p.categories join p.units")
+	@Override
+	public List<SupplierProducts> findAll();
 }
