2178123e330f59372ce5fdee84ce82c8047f526b
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-02-16 08:11:46 -0300

    feat: timezone config to utc

--- src/main/java/com/ssdms/SsdmsApplication.java
+++ src/main/java/com/ssdms/SsdmsApplication.java
@@ -1,5 +1,7 @@
 package com.ssdms;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SsdmsApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(SsdmsApplication.class, args);
 	}
 
