411a48044fa483e4d8eb3120d78a380c7e737674
Diorgenes Morais <diorgenesmorais@gmail.com>
2024-02-16 08:08:59 -0300

    feat: add copyToDomainObject method

--- src/main/java/com/ssdms/api/assembler/ProductInputDisassembler.java
+++ src/main/java/com/ssdms/api/assembler/ProductInputDisassembler.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.ssdms.api.model.input.ProductInput;
+import com.ssdms.domain.model.Category;
 import com.ssdms.domain.model.Products;
+import com.ssdms.domain.model.Unit;
 
 @Component
 public class ProductInputDisassembler {
@@ -15,5 +17,14 @@ public class ProductInputDisassembler {
 
 	public Products toDomainObject(ProductInput productInput) {
 		return modelMapper.map(productInput, Products.class);
+	}
+	
+	public void copyToDomainObject(ProductInput productInput, Products product) {
+		// Para evitar org.hibernate.HibernateException: identifier of an instance of 
+		// com.ssdms.domain.model.Cotegory was altered from 1 to 2
+		product.setCategory(new Category());
+		product.setUnit(new Unit());
+
+		modelMapper.map(productInput, product);
 	}
 }
